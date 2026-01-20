9d4f2676c81c6f2c9d6fe0147ea8721372348735
dirkarnez <smalldirkalex@gmail.com>
2026-01-20 22:06:45 +0800

    - proof of concept working

--- src/main.ts
+++ src/main.ts
@@ -20,12 +20,72 @@
 //   </div>
 // `
 
+
+const Friday = 5;
+
 ((appContainer: HTMLDivElement | null) => {
   if (!appContainer) {
     return;
   }
 
-  
+  [
+    { 
+      name: "EIE3105",
+      weekday: Friday,
+      startsAt: {
+        hour: 8,
+        minutes: 30
+      },
+      endsAt: {
+        hour: 11,
+        minutes: 30
+      }
+    }
+  ];
 
+
+  // Array.from({length: 7})
+
+  /*
+  <div class="container">
+    <div>1</div>
+    <div style="grid-area: 1 / 1 / 3 / 2;">2</div>
+    <div>3</div>
+    <div>4</div>
+    <div>5</div>
+  </div>
+  */
+  const timetableContainer: HTMLDivElement = document.createElement("div");
+  timetableContainer.classList.add("container");
+  {
+    Array
+    .from({length: 6})
+    .forEach((_, i) => {
+      const element = document.createElement("div");
+      element.innerText = `${i + 1}`;
+      element.style.gridArea = `1 / ${i + 1} / 2 / ${i + 2}`;
+      timetableContainer.appendChild(element);
+    });
+  }
+  
+  {
+    const startIndex = 2;
+    const weekday = 1;
+    const numOfHalfHoursSession = 4;
+    const lesson = document.createElement("div");
+    lesson.style.gridArea = `${startIndex} / ${weekday} / ${startIndex + numOfHalfHoursSession} / ${weekday + 1}`;
+    timetableContainer.appendChild(lesson);
+  }
+
+    {
+      const startIndex = 2;
+      const startOffset = 3;
+      const weekday = 2;
+      const numOfHalfHoursSession = 4;
+      const lesson = document.createElement("div");
+      lesson.style.gridArea = `${startIndex + startOffset} / ${weekday} / ${startIndex + startOffset + numOfHalfHoursSession} / ${weekday + 1}`;
+      timetableContainer.appendChild(lesson);
+    }
+  appContainer.appendChild(timetableContainer);
 
 })(document.querySelector<HTMLDivElement>('#app')!)
